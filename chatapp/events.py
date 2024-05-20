985683595a3c51dfb6f78b7e71ce611aebb1dfef
Tai <taiyebabatundejames@gmail.com>
2024-05-20 23:42:53 +0100

    update

--- chatapp/events.py
+++ chatapp/events.py
@@ -1,5 +1,10 @@
 from .extensions import socketio
 from flask_socketio import emit
+from flask import request
+
+users = {}
+
+
 @socketio.on("connect")
 def handle_connected():
     print ("client connected!")
@@ -8,8 +13,15 @@ def handle_connected():
 @socketio.on("user_join")
 def handle_user_join(username):
     print(f"User {username} joined")
+    users[username] = request.sid
+
+
 
 @socketio.on("new_message")
 def handle_new_message(message):
     print(f"New message: {message}")
-    emit("chat",{'message':message}, broadcast=True)
+    username = None
+    for user in users:
+        if users[user] == request.sid:
+            username = user
+    emit("chat", {'message':message, "username":username}, broadcast=True)
